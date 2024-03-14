1fb712cef9c2f7be511799f2f156aa093f211387
namMOC <huynhtrongnhan.hn@gmail.com>
2024-03-14 20:47:57 +0700

    Quartz sync: Mar 14, 2024, 8:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 nấm MỐC",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
